37468b9400da30088bcd6f3c111c744eb8f21436
crytodevkj <omuniverse823@gmail.com>
2022-06-23 22:34:08 -0500

    Add bump

--- programs/vault/src/lib.rs
+++ programs/vault/src/lib.rs
@@ -141,7 +141,7 @@ pub struct InitProposalsContext<'info> {
 #[derive(Accounts)]
 #[instruction(bump: u8)]
 pub struct CreateProposal<'info> {
-    #[account(init, seeds = [b"proposal".as_ref(), id.as_ref()], payer = signer, space = 8 + 69 + 4 + 5 + 8)]
+    #[account(init, seeds = [b"proposal".as_ref(), id.as_ref()], payer = signer, space = 8 + 69 + 4 + 5 + 8, bump)]
     pub proposal: Account<'info, Proposal>,
     /// CHECK:
     pub vault_account: AccountInfo<'info>,
