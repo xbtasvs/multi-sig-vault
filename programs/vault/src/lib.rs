56099f8509fca5fec62c6f701ad7aab2307fec13
crytodevkj <omuniverse823@gmail.com>
2022-06-24 12:45:04 -0500

    Fix typo

--- programs/vault/src/lib.rs
+++ programs/vault/src/lib.rs
@@ -25,7 +25,7 @@ pub mod vault {
 
     pub fn init_proposals(ctx: Context<InitProposalsContext>, _bump: u8) -> ProgramResult {
         let proposals = &mut ctx.accounts.proposals;
-        proposlas.created = [false;30];
+        proposals.created = [false;30];
         Ok(())
     }
 
