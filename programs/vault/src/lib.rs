2fa2b56b72139ffc93efaf790425dd8d64d490ac
crytodevkj <omuniverse823@gmail.com>
2022-06-26 20:08:40 -0500

    Fix typo

--- programs/vault/src/lib.rs
+++ programs/vault/src/lib.rs
@@ -58,7 +58,7 @@ pub mod vault {
         signed[index] = true;
         proposal.id = id;
         proposal.signed = signed;
-        proposal.recipient = ctx.account.recipient;
+        proposal.recipient = ctx.accounts.recipient;
         proposal.amount = 0xffffffff * amount1 as u64 + amount2 as u64;
         proposals.created.push(id);
         Ok(())
